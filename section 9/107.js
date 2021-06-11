bca16a25ac8f32879e3d0a394fb1fc4afb8d53d3
Destoky <adestoky@gmail.com>
2021-06-11 18:38:12 +0200

    107. Short Circuiting (&& and ||)

---
+++ section 9/107.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// console.log(3 || "Jean");
+// console.log(undefined || null);
+
+// if([]) {
+//   console.log("dans true");
+// } else {
+//   console.log("dans false");
+// }
+
