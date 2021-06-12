12ce2edc1ca23cf958a7d1ac0c4475dae73bb685
Destoky <adestoky@gmail.com>
2021-06-12 10:41:43 +0200

    111. Enhanced Object Literals

---
+++ section 9/111.js
@@ -0,0 +1,24 @@
+const domicile =  {
+  village : "Compogne",
+  commune : "Bertogne",
+  rue : "Vivrou"
+}
+
+const personnage = {
+  identifiant : {
+    prenom : "Alexandre",
+    nom :"Destoky"
+  },
+  age : 24,
+  domicile,
+  presentation() {
+    console.log(`Bonjour, je m'appelle ${this.identifiant.prenom} ${this.identifiant.nom}`);
+  }
+}
+
+
+// for (const el of personnage) {
+//   console.log(el);
+// }
+
+personnage.presentation();
