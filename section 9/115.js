bfa83e40fc1c971e347a6bc43bc88888649d5047
Destoky <adestoky@gmail.com>
2021-06-13 20:44:53 +0200

    115. Sets

---
+++ section 9/115.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const prenoms = ["Jean","Thomas","Marc","Thomas","Jean","Andre","Jules","Marc","Jean"];
+
+let test = new Set(prenoms);
+test = [...test];
+
+console.log(test);
+
+const x = new Set("alexandre");
+
+console.log(x);
+
