c41b0359793536eeef1ea5f7c662264cb9fbf0c6
Destoky <adestoky@gmail.com>
2021-06-14 11:37:40 +0200

    117. Maps : Iteration

---
+++ section 9/117.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
