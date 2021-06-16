5e1b7973b2bf88dbf74a60675f85086104967e98
Destoky <adestoky@gmail.com>
2021-06-16 23:20:22 +0200

    131. Functions Returning Functions

--- section 10/131.js
+++ section 10/131.js
@@ -1,1 +1,28 @@
 "use strict";
+
+// const presentation = function (salutations) {
+//   return function (nom) {
+//     console.log(`${salutations} ${nom}`);
+//   };
+// };
+
+const presentation = salutations=> nom=> console.log(`${salutations} ${nom}`) 
+
+
+
+
+
+let fctBonjour = presentation("Bonjour");
+let fctAurevoir = presentation("Aurevoir");
+console.log(fctBonjour);
+
+fctBonjour("Patrick");
+fctAurevoir("Benoit");
+
+
+const presen = function (salutations,nom) {
+    console.log(`${salutations} ${nom}`);
+};
+
+presen("Aurevoir","Jean");
+
