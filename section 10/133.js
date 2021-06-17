dc0f22db69f9a238911845eee3ddea0b5b14ae5e
Destoky <adestoky@gmail.com>
2021-06-17 13:21:06 +0200

    133. The bind Method

--- section 10/133.js
+++ section 10/133.js
@@ -1,1 +1,39 @@
 "use strict";
+const magicien = {
+  nom: "Gandalf",
+  age: 115,
+  dommage: [],
+};
+const archer = {
+  nom: "Legolas",
+  age: 215,
+  dommage: [],
+};
+
+const attaque = function (degats, ennemi) {
+  console.log(`${this.nom} à infligé ${degats} dommage à ${ennemi}`);
+  this.dommage.push({ degats, ennemi });
+};
+
+
+
+
+
+// const salut = function() {
+//   console.log("Salutions !");
+// }
+
+let para = document.createElement("button");
+para.textContent = "test";
+document.body.append(para);
+
+let btn = document.querySelector("button");
+
+
+btn.addEventListener("click", attaque.bind(magicien,140,"Yoda"));
+
+
+const magicienVSYoda = attaque.bind(magicien,140);
+
+
+magicienVSYoda("Yoda")
