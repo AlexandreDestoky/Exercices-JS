fa32a5a84fec73b4d882b17d1855097498c5d0df
Destoky <adestoky@gmail.com>
2021-06-17 17:15:29 +0200

    136. Closures

--- section 10/136.js
+++ section 10/136.js
@@ -1,1 +1,42 @@
 "use strict";
+
+const augmentation = function () {
+  let cpt = 0;
+  return function () {
+    cpt++;
+    console.log(cpt);
+  };
+};
+
+let compteur1 = augmentation();
+let compteur2 = augmentation();
+compteur1();
+compteur1();
+
+compteur2();
+compteur2();
+compteur2();
+compteur2();
+
+compteur1();
+compteur1();
+compteur1();
+compteur1();
+
+compteur2();
+
+compteur2();
+// let multiplicateur = 7;
+
+// const double = function (nbr) {
+//   // const multiplicateur = 2;
+//   return nbr * multiplicateur;
+// };
+
+// const triple = function (nbr) {
+//   const multiplicateur = 3;
+//   return nbr * multiplicateur;
+// };
+
+// console.log(multiplicateur);
+// console.log(double(18));
