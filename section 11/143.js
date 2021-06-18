6ce286f30803ab01c4de2594939ce625e7371f87
Destoky <adestoky@gmail.com>
2021-06-18 13:58:30 +0200

    143. forEach With Maps and Sets

--- section 11/143.js
+++ section 11/143.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+
+currencies.forEach(function(el,i,arr) {
+  console.log(`${i} : ${el}`);
+})
+
+const currenciesUnique = new Set(["USD","EUR","GBP","USD","YEN","EUR"]);
+console.log(currenciesUnique);
+
+
+currenciesUnique.forEach(function(el,_,i){
+  console.log(`${i} : ${el}`);
+})
