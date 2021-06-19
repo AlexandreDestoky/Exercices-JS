9423c332a810d456ccda8b982f749478108c9717
Destoky <adestoky@gmail.com>
2021-06-19 22:55:14 +0200

    150. The filter Method

--- section 11/150.js
+++ section 11/150.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const tabNombres = [200, 450, -25, 3000, -650, 130, 425, -24];
+
+const tabFilter = tabNombres.filter((x) => x > 100);
+console.log(tabFilter);
+
+const tabMap = tabNombres.map(x => x > 100 ? "❤":"💨");
+console.log(tabMap);
