ec8ec84cb5ab9f52072394fd799e6ea47dee4d62
Destoky <adestoky@gmail.com>
2021-06-19 17:50:43 +0200

    147. Data Transformations : map, filter, reduce

--- section 11/147.js
+++ section 11/147.js
@@ -1,1 +1,7 @@
 "use strict";
+
+// Map => lance opération sur chaque élément dans nouveau tableau
+
+// Filter => Filtre les élements qui passe le test dans un nouveau tableau
+
+// Reduce => Réduit des élements en une seule valeur (compte age total de personnes)
