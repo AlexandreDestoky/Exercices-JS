67ea574f9d5f98eb099e80ec44d6629562632468
Destoky <adestoky@gmail.com>
2021-06-19 22:13:52 +0200

    148. The map Method

--- section 11/148.js
+++ section 11/148.js
@@ -1,1 +1,34 @@
 "use strict";
+
+// let points = [7, 4, 5, 9, 10, 2, 3, 4, 4, 5, 8];
+
+// let points20 = [];
+
+// for (const point of points) {
+//   points20.push(point * 2);
+// }
+
+
+// let pointsSur20 = points.map(x => x * 2);
+
+
+// console.log(points20);
+// console.log(pointsSur20);
+
+let tva = 1.21;
+
+let montant = [7, 4, 5, 9, 10, 2, 3, 4, 4, 5, 8];
+
+// const convTva = montant.map(x => x*1.21);
+// console.log(convTva);
+
+// const convTva2 = montant.map(function(el) {
+//   return el * tva;
+// })
+// console.log(convTva2);
+
+montant.forEach(function(el) {
+  console.log(el);
+})
+
+montant.forEach(x => console.log(x));
