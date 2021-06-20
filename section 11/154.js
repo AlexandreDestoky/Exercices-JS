ab47d10ad0478507590506b0e7e6871e327e14da
Destoky <adestoky@gmail.com>
2021-06-20 19:50:57 +0200

    154. Coding Challenge #3

--- section 11/154.js
+++ section 11/154.js
@@ -1,1 +1,25 @@
 "use strict";
+
+// Coding Challenge #3
+
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, 
+but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = ages => {
+  let humanAge = ages.map(x => (x <= 2 ? (x *= 2) : (x = 16 + 4 * x)));
+  // console.log(humanAge);
+  humanAge = humanAge.filter(x => x >= 18);
+  // console.log(humanAge);
+  return humanAge.reduce((acc, x) => acc + x) / humanAge.length;
+  // return humanAge.reduce((acc,x) => acc + (x / humanAge.length),0); AUTRE SOLUTION
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
