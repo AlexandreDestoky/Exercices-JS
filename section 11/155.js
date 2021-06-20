2140c9410edcdbf34f224042899f6a9cc82d9d04
Destoky <adestoky@gmail.com>
2021-06-20 21:24:28 +0200

    155. The find Method

--- section 11/155.js
+++ section 11/155.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const notes = [4,5,6,8,13,4,5,26,28,20];
+
+const x = notes.find(x => x > 10);
+
+console.log(x);
+
+const perso = [
+  {
+    prenom:"Gandalf",
+    age: 114
+  },
+  {
+    prenom:"Gimli",
+    age: 80
+  },
+  {
+    prenom:"Legolas",
+    age: 300
+  },
+]
+
+const vieux = perso.find(x => x.age > 200);
+console.log(vieux);
