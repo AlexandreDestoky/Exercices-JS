ea62b0d8ab6929bc2e602effccb7b353714b1b95
Destoky <adestoky@gmail.com>
2021-06-21 10:08:23 +0200

    158. The findIndex Method

--- section 11/158.js
+++ section 11/158.js
@@ -1,1 +1,12 @@
 "use strict";
+
+
+const notes = [4,5,6,8,13,4,5,26,28,20];
+
+const x = notes.find(x => x > 10);
+console.log(x);
+
+const y = notes.findIndex(x => x > 10);
+console.log(y);
+
+console.log(notes.findIndex(x => x === 5));
