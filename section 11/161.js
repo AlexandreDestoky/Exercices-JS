ddb4961f846f2ed49a5d41c04ff72969d4c6fcbd
Destoky <adestoky@gmail.com>
2021-06-21 15:17:11 +0200

    161. Sorting Arrays

--- section 11/161.js
+++ section 11/161.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const tabPrenom = ["Jean", "Benoit", "Bruno", "Andre", "Pierre", "Alexandre", "Patrick", "Luc"];
+console.log(tabPrenom);
+console.log(tabPrenom.sort());
+
+const notes = [4, 5, 6, 18, 15, 19, 16, 14, 13, 134, 15, 18];
+console.log(notes);
+console.log(notes.sort());
+console.log(notes.sort((a, b) => a - b));
