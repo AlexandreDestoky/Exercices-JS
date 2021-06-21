17113668bb8e43e6a2034d30d4dc9b8ae27e3f2f
Destoky <adestoky@gmail.com>
2021-06-21 10:17:34 +0200

    159. some And every

--- section 11/159.js
+++ section 11/159.js
@@ -1,1 +1,16 @@
 "use strict";
+
+let notes = [4, 6, 8, 6, 15, 16, 4, 18, 3, 9, 17, 17, 15];
+
+console.log(notes.includes(19));
+
+// Teste si au moins 1 élément passe le test 
+console.log(notes.some(x => x > 15));
+console.log(notes.some(x => x > 25));
+console.log(notes.some(x => x < 4));
+console.log("--------------------------");
+
+// Teste si touts les élements passe le test
+console.log(notes.every(x => x > 2));
+console.log(notes.every(x => x > 5));
+console.log(notes.every(x => x > 2 && x < 19));
