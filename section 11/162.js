49e1f7ce4c30a6ab4328f958d22092f2d9d49230
Destoky <adestoky@gmail.com>
2021-06-21 15:59:13 +0200

    162. More Ways of Creating and Filling Arrays

--- section 11/162.js
+++ section 11/162.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let chiffres = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(chiffres);
+
+let x = new Array(7);
+console.log(x);
+x.fill(0);
+console.log(x);
+
+x.fill(5,3,5);
+console.log(x);
+
+const t = Array.from("Test");
+console.log(t);
