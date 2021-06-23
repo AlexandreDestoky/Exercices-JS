08ac5555f116f168e60dd96eca2a444f2d66201a
Destoky <adestoky@gmail.com>
2021-06-23 11:14:08 +0200

    170. The Remainder Operator (%)

--- section 12/170.js
+++ section 12/170.js
@@ -1,1 +1,23 @@
 "use strict";
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 2);
+
+const paire = nombre => nombre % 2 === 0;
+
+console.log(paire(2));
+
+
+
+const jeuBottle = limite => {
+  const sumDigits = n => n.toString().split("").map(Number).reduce((a,b) => a + b);
+  for (let i = 1; i <= limite; i++) {
+    console.log(i % 10 === 7 || i % 7 === 0 || sumDigits(i) === 7 ? "Bottle" : i );
+  }
+};
+
+console.log("---------------------");
+
+jeuBottle(1000);
