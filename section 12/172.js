45f38484f74b9a692da31f2be51e04e9e27c5ccf
Destoky <adestoky@gmail.com>
2021-06-23 12:22:00 +0200

    172. Creating Dates

--- section 12/172.js
+++ section 12/172.js
@@ -1,1 +1,14 @@
 "use strict";
+
+console.log(new Date());
+console.log(new Date("Jun 21 2021 12:07:14"));  
+console.log(Date.now()); 
+
+console.log(new Date().getFullYear());
+console.log(new Date().getMonth()+1);
+console.log(new Date().getDate());
+
+let today = new Date();
+today.setFullYear(2022);
+
+console.log(today);
