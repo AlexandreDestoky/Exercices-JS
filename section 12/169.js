7af13893f3d2ebe36cb741e6c92fd9283f0ca48a
Destoky <adestoky@gmail.com>
2021-06-23 10:41:51 +0200

    169. Math and Rounding

--- section 12/169.js
+++ section 12/169.js
@@ -1,1 +1,21 @@
 "use strict";
+
+console.log(Math.sqrt(25));
+
+console.log(9 ** 2);
+
+console.log(Math.max(5, 8, 12, 4, 6));
+console.log(Math.min(5, 8, 12, 4, 6));
+
+console.log(3.1415926535897932384626433 - Math.PI);
+
+console.log(Math.floor(Math.random() * 11)); //9.99
+
+const randomMinMax = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+console.log(randomMinMax(4,6));  
+
+
+console.log((2.7).toFixed(2));
+console.log((2.7).toFixed(0));
+console.log((2.3).toFixed(0));
