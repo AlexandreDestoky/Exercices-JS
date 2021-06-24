09f8598ec015b99eb68253104f30a2e614168f0d
Destoky <adestoky@gmail.com>
2021-06-24 12:05:50 +0200

    176. Internationalizing Numbers (Intl)

--- section 12/176.js
+++ section 12/176.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const num = 468786786.55;
+
+console.log("US",new Intl.NumberFormat("en-us").format(num));
+console.log("BE",new Intl.NumberFormat("fr-be").format(num));
+console.log("BE",new Intl.NumberFormat(navigator.language).format(num));
