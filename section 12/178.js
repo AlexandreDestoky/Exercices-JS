a107ca2083df2eb86b5a07703a2a2075dd9a74c0
Destoky <adestoky@gmail.com>
2021-06-25 20:06:41 +0200

    178. Implementing a Countdown Timer

--- section 12/178.js
+++ section 12/178.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const compteur = function() {
+  seconde.textContent = Number(seconde.textContent) + 1;
+  Number(seconde.textContent) < 10 ? seconde.textContent = "0" + seconde.textContent  : "";
+  if(Number(seconde.textContent) > 59) {
+    seconde.textContent = 0;
+    minute.textContent = Number(minute.textContent) + 1;
+    Number(minute.textContent) < 10 ? minute.textContent = "0" + minute.textContent  : "";
+    if(minute.textContent > 59) {
+      minute.textContent = 0;
+      heure.textContent = Number(heure.textContent) + 1 
+    }
+  }
+}
