e3330043c28f7a6170d8f566ccafdfc95af78a83
Destoky <adestoky@gmail.com>
2021-07-02 10:56:17 +0200

    198. Building a Slider Component : Part 2

--- Bankist Website/script.js
+++ Bankist Website/script.js
@@ -266,27 +266,74 @@ const imgObserver = new IntersectionObserver(loadImg, {
 imgTargets.forEach(img => imgObserver.observe(img));
 
 // -------------- 197 --------------
+// Selection DOM
 const slides = document.querySelectorAll(".slide");
 const btnGauche = document.querySelector(".slider__btn--left");
 const btnDroite = document.querySelector(".slider__btn--right");
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+const dotContainer = document.querySelector(".dots");
 
+// Variables de travail
 let curSlide = 0;
 const maxSlide = slides.length;
 
+//Créer les points du diaporama
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML("beforeend", `<button class="dots__dot" data-slide="${i}"></button>`);
+  });
+};
+
+const activateDot = function (slide) {
+  document.querySelectorAll(".dots__dot").forEach(dot => dot.classList.remove("dots__dot--active"));
+  
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add("dots__dot--active");
+};
+
+//Créer Diapo Début
 const goToSlide = function (slide) {
   slides.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
 };
-goToSlide(0);
 
+//Prochaine Slide
 const nextSlide = function () {
   curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
+
+// const init = function() {
+//   goToSlide(0);
+//   createDots();
+//   activateDot(0);
+// }
+// init();
+
+//Fonction une utilisation IIFE
+(function(){
+  goToSlide(0);
+  createDots();
+  activateDot(0);
+})();
+
+//Précédente Slide
 const prevSlide = function () {
   curSlide === 0 ? (curSlide = maxSlide - 1) : curSlide--;
   goToSlide(curSlide);
+  activateDot(curSlide);
 };
 
+// Events
 btnDroite.addEventListener("click", nextSlide);
 btnGauche.addEventListener("click", prevSlide);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") prevSlide();
+  if (e.key === "ArrowRight") nextSlide();
+});
+
+dotContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("dots__dot")) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activateDot(slide);
+  }
+});
