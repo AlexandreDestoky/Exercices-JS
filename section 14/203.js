c4060d419f7ea464736ae32659decc2bb2e3e4dc
Destoky <adestoky@gmail.com>
2021-07-02 17:15:59 +0200

    203. What is Object-Oriented Programming ?

---
+++ section 14/203.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Abstraction 
+// Pas besoin de savoir comment ça marche pour utiliser
+
+// Encapsulation 
+// Propriété et méthodes peuvent pas être changer en dehord de classe
+
+// Héritage
+// Hérite propriétés communes
+
+// Polymorphism
+// Une classe enfant peut overwrite une methode de son parent
