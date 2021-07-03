f2e61d156e3d0607073aef634deb8e94df983959
Destoky <adestoky@gmail.com>
2021-07-03 22:39:53 +0200

    205. Constructor Functions and the new Operator

--- section 14/205.js
+++ section 14/205.js
@@ -1,1 +1,15 @@
 "use strict";
+// Convention majuscule pour constructeur
+
+// Méthode grand mère
+const Person = function (nom, prenom, age) {
+  this.nom = nom;
+  this.prenom = prenom;
+  this.age = age;
+};
+
+const moi = new Person("Destoky", "Alexandre", 24);
+const moi2 = new Person("Destoky", "Alex", 25);
+
+console.log(moi);
+console.log(moi2);
