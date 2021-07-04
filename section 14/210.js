1e926ea1e036d59e78dd09373db71d4f81517c68
Destoky <adestoky@gmail.com>
2021-07-04 20:23:22 +0200

    210. ES6 Classes

--- section 14/210.js
+++ section 14/210.js
@@ -1,1 +1,21 @@
 "use strict";
+
+class Person {
+  constructor(nom, prenom, age) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+  }
+  anneeNaissance() {
+    console.log(2021 - this.age);
+    return 2021 - this.age;
+  }
+  presentation() {
+    console.log(`Bonjour, je m'appelle ${this.prenom} ${this.nom} et je suis né en ${this.anneeNaissance()} `);
+  }
+}
+
+const alexandre = new Person("Destoky", "Alexandre", 24);
+
+alexandre.anneeNaissance();
+alexandre.presentation();
