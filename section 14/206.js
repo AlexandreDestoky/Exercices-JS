aabf9323da4416602d0a00175c28224be2284b29
Destoky <adestoky@gmail.com>
2021-07-04 11:28:43 +0200

    206. Prototypes

--- section 14/206.js
+++ section 14/206.js
@@ -1,1 +1,14 @@
 "use strict";
+const Person = function (nom, prenom, age) {
+  this.nom = nom;
+  this.prenom = prenom;
+  this.age = age;
+};
+
+Person.prototype.calcAge = function() {
+  console.log(2021 - this.age);
+}
+
+const moi = new Person("Destoky", "Alexandre", 24);
+
+// moi.age.calcAge()
