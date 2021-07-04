b64b97f6de8a4548a45e32ca5d08d10d42a040ec
Destoky <adestoky@gmail.com>
2021-07-04 21:03:00 +0200

    211. Setters and Getters

--- section 14/211.js
+++ section 14/211.js
@@ -1,1 +1,44 @@
 "use strict";
+
+class Person {
+  constructor(nom, prenom, age) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.famille;
+  }
+  // Methods
+  anneeNaissance() {
+    console.log(2021 - this.age);
+    return 2021 - this.age;
+  }
+  presentation() {
+    console.log(`Bonjour, je m'appelle ${this.prenom} ${this.nom} et je suis né en ${this.anneeNaissance()} `);
+  }
+  // getters
+  getAge() {
+    return this.age;
+  }
+  // setters
+  set nom(el) {
+    this._nom = el;
+    this._famille = el === "Destoky";
+  }
+
+  get nom() {
+    return this._nom;
+  }
+
+}
+
+
+const alexandre = new Person("Destoky", "Alexandre", 24);
+// alexandre.anneeNaissance();
+// alexandre.presentation();
+console.log(alexandre.age);
+console.log(alexandre.getAge());
+// alexandre.setNom("Destoky");
+console.log(alexandre._nom);
+console.log(alexandre.famille);
+
+console.log(alexandre);
