50f355bcbd8f5a6f1fee6ad4aec53674f9f6e200
Destoky <adestoky@gmail.com>
2021-07-05 14:05:58 +0200

    219. Another Class Example

--- section 14/219.js
+++ section 14/219.js
@@ -1,2 +1,27 @@
 "use strict";
 
+class Compte {
+  constructor(proprietaire, devise, code) {
+    this.proprietaire = proprietaire;
+    this.devise = devise;
+    this.code = code;
+    this.mouvements = [];
+  }
+  ajout(montant) {
+    this.mouvements.push(montant)
+  }
+  retrait(montant) {
+    this.ajout(-montant);
+  }
+}
+
+const compte1 = new Compte("Alexandre", "EUR", 5432);
+compte1.ajout(500);
+compte1.ajout(800);
+compte1.retrait(400);
+
+console.log(compte1.mouvements);
+compte1.mouvements.push(200);
+console.log(compte1.mouvements);
+
+
