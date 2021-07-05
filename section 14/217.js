bf6ff94949522063cabbf22730c85a0604f2473e
Destoky <adestoky@gmail.com>
2021-07-05 10:30:52 +0200

    217. Inheritance Between "Classes" : ES6 Classes

--- section 14/217.js
+++ section 14/217.js
@@ -1,1 +1,34 @@
 "use strict";
+
+class Person {
+  constructor(nom, prenom, age) {
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+    this.famille;
+  }
+  anneeNaissance() {
+    return 2021 - this.age;
+  }
+  presentation() {
+    console.log(`Bonjour, je m'appelle ${this.prenom} ${this.nom} et je suis né en ${this.anneeNaissance()} `);
+  }
+  salut() {
+    console.log("Salut 🤗");
+  }
+}
+
+class Student extends Person{
+  constructor(nom,prenom,age,cours) {
+    super(nom,prenom,age);
+    this.cours = cours;
+  }
+  salut() {
+    console.log("Salut dans sous-classe");
+  }
+}
+
+
+const jona = new Student("Bastogne","Jonathan",23,"Sport");
+jona.presentation();
+jona.salut();
