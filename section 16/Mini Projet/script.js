ae1554a510a249d0ccb6c464302c562cced7955f
Destoky <adestoky@gmail.com>
2021-07-12 17:54:49 +0200

    246. Welcome to CallBack Hell

--- section 16/Mini Projet/script.js
+++ section 16/Mini Projet/script.js
@@ -4,7 +4,28 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-const dataPays = function(pays) {
+
+
+
+const affichePays = function(data,className = "") {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)} M</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.map(el => el.name)}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.map(el => el.name)}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML("beforeend",html);
+  countriesContainer.style.opacity = 1;
+}
+
+const dataPaysEtVoisin = function(pays) {
+  // AJAX APPELLE PAYS 1
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.eu/rest/v2/name/${pays}`);
   request.send();
@@ -13,25 +34,25 @@ const dataPays = function(pays) {
     // console.log(this.responseText);
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-    console.log(data.languages);
-    const html = `
-    <article class="country">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)} M</p>
-        <p class="country__row"><span>🗣️</span>${data.languages.map(el => el.name)}</p>
-        <p class="country__row"><span>💰</span>${data.currencies.map(el => el.name)}</p>
-      </div>
-    </article>
-    `;
-    countriesContainer.insertAdjacentHTML("beforeend",html);
-    countriesContainer.style.opacity = 1;
+    affichePays(data);
+    //Voisin  (PAYS 2)
+    const [voisin] = data.borders;
+    if(!voisin) return;
+    // AJAX APPELLE PAYS 2
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.eu/rest/v2/alpha/${voisin}`);
+    request2.send();
+    request2.addEventListener("load",function() {
+      const data2 = JSON.parse(this.responseText)
+      affichePays(data2,"neighbour");
+      console.log(this);
+    })
+
+
   });
 }
 
-dataPays("belgique");
-dataPays("France");
-// dataPays("Italia");
-dataPays("Suisse");
+dataPaysEtVoisin("SWEDEN");
+dataPaysEtVoisin("France");
+dataPaysEtVoisin("Congo");
+// dataPaysEtVoisin("GB");
