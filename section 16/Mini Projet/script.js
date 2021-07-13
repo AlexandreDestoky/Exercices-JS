4cd5624a48b3a19620d5b293131d3c61fcaa3b0e
Destoky <adestoky@gmail.com>
2021-07-13 12:55:35 +0200

    247. Promises and the Fetch API

--- section 16/Mini Projet/script.js
+++ section 16/Mini Projet/script.js
@@ -1,58 +1,70 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
+// const btn = document.querySelector(".btn-country");
+// const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
+// ///////////////////////////////////////
 
 
 
-const affichePays = function(data,className = "") {
-  const html = `
-  <article class="country ${className}">
-    <img class="country__img" src="${data.flag}" />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)} M</p>
-      <p class="country__row"><span>🗣️</span>${data.languages.map(el => el.name)}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.map(el => el.name)}</p>
-    </div>
-  </article>
-  `;
-  countriesContainer.insertAdjacentHTML("beforeend",html);
-  countriesContainer.style.opacity = 1;
-}
+// const affichePays = function(data,className = "") {
+//   const html = `
+//   <article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(2)} M</p>
+//       <p class="country__row"><span>🗣️</span>${data.languages.map(el => el.name)}</p>
+//       <p class="country__row"><span>💰</span>${data.currencies.map(el => el.name)}</p>
+//     </div>
+//   </article>
+//   `;
+//   countriesContainer.insertAdjacentHTML("beforeend",html);
+//   countriesContainer.style.opacity = 1;
+// }
 
-const dataPaysEtVoisin = function(pays) {
-  // AJAX APPELLE PAYS 1
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.eu/rest/v2/name/${pays}`);
-  request.send();
+// const dataPaysEtVoisin = function(pays) {
+//   // AJAX APPELLE PAYS 1
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.eu/rest/v2/name/${pays}`);
+//   request.send();
   
-  request.addEventListener("load", function () {
-    // console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    affichePays(data);
-    //Voisin  (PAYS 2)
-    const [voisin] = data.borders;
-    if(!voisin) return;
-    // AJAX APPELLE PAYS 2
-    const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.eu/rest/v2/alpha/${voisin}`);
-    request2.send();
-    request2.addEventListener("load",function() {
-      const data2 = JSON.parse(this.responseText)
-      affichePays(data2,"neighbour");
-      console.log(this);
-    })
+//   request.addEventListener("load", function () {
+//     // console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     affichePays(data);
+//     //Voisin  (PAYS 2)
+//     const [voisin] = data.borders;
+//     if(!voisin) return;
+//     // AJAX APPELLE PAYS 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open("GET", `https://restcountries.eu/rest/v2/alpha/${voisin}`);
+//     request2.send();
+//     request2.addEventListener("load",function() {
+//       const data2 = JSON.parse(this.responseText)
+//       affichePays(data2,"neighbour");
+//       console.log(this);
+//     })
 
 
-  });
-}
+//   });
+// }
 
-dataPaysEtVoisin("SWEDEN");
-dataPaysEtVoisin("France");
-dataPaysEtVoisin("Congo");
-// dataPaysEtVoisin("GB");
+// dataPaysEtVoisin("SWEDEN");
+// dataPaysEtVoisin("France");
+// dataPaysEtVoisin("Congo");
+// // dataPaysEtVoisin("GB");
+
+
+
+
+// Méthode XMLHttpRequest 
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.eu/rest/v2/name/${pays}`);
+//   request.send();
+
+// Méthode Fetch
+const request = fetch(`https://restcountries.eu/rest/v2/name/france`);
+console.log(request);
