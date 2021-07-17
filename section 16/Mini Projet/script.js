97ed988605b65f74c56b912ee8ac7f8c239551fb
Destoky <adestoky@gmail.com>
2021-07-17 23:31:57 +0200

    253. Asynchronous Behind the Scenes : The Event Loop

--- section 16/Mini Projet/script.js
+++ section 16/Mini Projet/script.js
@@ -116,7 +116,33 @@ const dataPaysEtVoisin = function (pays) {
     
 };
 
-dataPaysEtVoisin("belgique");
-dataPaysEtVoisin("Australia");
+// dataPaysEtVoisin("belgique");
+// dataPaysEtVoisin("Australia");
 // dataPaysEtVoisin("spain");
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json&auth=645697531954568548632x24006`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => affichePays(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
+
 
