014cecc039e12cb87073e6597ec35a5a08cefff0
Destoky <adestoky@gmail.com>
2021-07-18 23:17:48 +0200

    254. The Event Loop in Practice

--- section 16/254.js
+++ section 16/254.js
@@ -1,1 +1,8 @@
 "use strict";
+
+console.log("test start");
+setTimeout(()=> console.log("0 sec timer"),0);
+
+Promise.resolve("Resolved promise 1").then(res => console.log(res));
+Promise.resolve("Resolved promise 2").then(res => console.log(res));
+console.log("test end");
