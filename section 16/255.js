5b67833e834d8a32520ab8b5312111ae20b28fc5
Destoky <adestoky@gmail.com>
2021-07-19 10:57:41 +0200

    255. Building a Simple Promise

--- section 16/255.js
+++ section 16/255.js
@@ -1,1 +1,59 @@
 "use strict";
+
+const lotteryPromesse = new Promise(function (resolve, reject) {
+  console.log("tirage au sort 😁");
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve("Vous avez gagné 🎉 !");
+    } else {
+      reject(new Error("Vous avez perdu 😭 !"));
+    }
+  }, 2000);
+});
+
+lotteryPromesse.then(res => console.log(res)).catch(err => console.error(err));
+
+
+const wait = function (secondes) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve,secondes * 1000)
+  })
+};
+
+
+// wait(4).then(() => {
+//   console.log("Wait fini 4secondes!");
+//   return wait(3);
+// }).then(() => console.log("Wait fini 3 secondes plus tard"))
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => console.log('4 second passed'));
+
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 second passed');
+//       setTimeout(() => {
+//         console.log('4 second passed');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+
+Promise.resolve("abc").then(x => console.log(x));
+Promise.reject("abc").catch(x => console.log(x));
