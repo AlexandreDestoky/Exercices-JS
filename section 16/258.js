e73468a3d8e99637015d2ab253fb4e444215ae54
Destoky <adestoky@gmail.com>
2021-07-22 09:31:46 +0200

    258. Consuming Promises with Async/Await

--- section 16/258.js
+++ section 16/258.js
@@ -1,1 +1,14 @@
 "use strict";
+
+
+const whereAmI = async function(country) {
+  const res =  await fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+  console.log(res);
+  const data = await res.json();
+  console.log(data);
+
+}
+
+whereAmI("portugal");
+console.log("First");
+
