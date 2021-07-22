1f5ca6ac7ea80178e0f507c695a3c128572f6439
Destoky <adestoky@gmail.com>
2021-07-22 09:57:26 +0200

    260. Returning Values from Async Functions

--- section 16/260.js
+++ section 16/260.js
@@ -1,1 +1,11 @@
 "use strict";
+
+(async function() {
+  try {
+    const city = await whereAmI();
+    console.log(city);
+  } catch(err) {
+    console.log(err);
+  }
+  console.log("Fini");
+})();
