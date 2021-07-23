4c9e6bb1f6745842005a570ae4cf14edfcc0019a
Destoky <adestoky@gmail.com>
2021-07-23 11:43:00 +0200

    274. Configuring Babel and Polyfilling

--- section_17/mini-projet/script.js
+++ section_17/mini-projet/script.js
@@ -18,32 +18,32 @@
 
 // console.log(cart);
 
-// const ShoppingCart2 = (function () {
-//   const cart = [];
-//   const shippingCost = 10;
-//   const totalPrice = 237;
-//   const totalQuantity = 23;
-//   const addToCart = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} added to cart`);
-//   };
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
 
-//   const orderStock = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(`${quantity} ${product} order from supplier`);
-//   };
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} order from supplier`);
+  };
 
-//   return {
-//     addToCart,
-//     cart,
-//     totalPrice,
-//     totalQuantity
-//   }
-// })();
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity
+  }
+})();
 
 
-// ShoppingCart2.addToCart("banana",24);
-// ShoppingCart2.addToCart("cherry",4);
+ShoppingCart2.addToCart("banana",24);
+ShoppingCart2.addToCart("cherry",4);
 // console.log(ShoppingCart2.shippingCost);
 
 //Export Modules en Node JS mais pas utile en JS
@@ -68,3 +68,27 @@ console.log(state);
 console.log(stateDeepClone);
 stateDeepClone.user.loggedIn = false;
 console.log(stateDeepClone);
+
+
+
+if(module.hot) {
+  module.hot.accept();
+}
+
+
+class Person {
+  #greeting = "Hey";
+  constructor(name) {
+    this.name= name;
+    console.log(`${this.#greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person("Alex");
+
+
+console.log(ShoppingCart2.cart.find(el => el.quantity >= 2));
+
+
+import "core-js/stable";
+import "regenerator-runtime/runtime";
