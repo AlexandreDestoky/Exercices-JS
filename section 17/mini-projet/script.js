0c977d393c68bb87c1ca5140a1447302bf21b356
Destoky <adestoky@gmail.com>
2021-07-23 10:25:26 +0200

    272. Introduction to NPM

--- section 17/mini-projet/script.js
+++ section 17/mini-projet/script.js
@@ -6,46 +6,65 @@
 
 // import * as ShoppingCart from "./shoppingCart.js";
 
-console.log("Module d'import");
+// console.log("Module d'import");
 
 // ShoppingCart.addToCart("bread",8);
 
-import add, { cart } from "./shoppingCart.js";
-add("bread", 45);
-add("apple", 49);
-add("pear", 15);
-add("tomato", 25);
+// import add, { cart } from "./shoppingCart.js";
+// add("bread", 45);
+// add("apple", 49);
+// add("pear", 15);
+// add("tomato", 25);
 
-console.log(cart);
+// console.log(cart);
 
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
 
-  const orderStock = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} order from supplier`);
-  };
+//   const orderStock = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} order from supplier`);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity
-  }
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity
+//   }
+// })();
 
 
-ShoppingCart2.addToCart("banana",24);
-ShoppingCart2.addToCart("cherry",4);
-console.log(ShoppingCart2.shippingCost);
+// ShoppingCart2.addToCart("banana",24);
+// ShoppingCart2.addToCart("cherry",4);
+// console.log(ShoppingCart2.shippingCost);
 
 //Export Modules en Node JS mais pas utile en JS
 
 // cd , dir , cls
+
+import cloneDeep from "../../node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+  cart: [
+    {product:"bread",quantity:5},
+    {product:"pizza",quantity:2},
+  ],
+  user : {loggedIn:true}
+};
+// const stateClone = Object.assign({},state);
+// state.user.loggedIn = false;
+// console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+console.log(state);
+console.log(stateDeepClone);
+stateDeepClone.user.loggedIn = false;
+console.log(stateDeepClone);
